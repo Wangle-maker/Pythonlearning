0d9c0513482be2b605bf9a563005aa7ff8b63f9b
Threshyong <1047981230@qq.com>
2020-12-22 16:31:33 +0800

    添加了新的文件，列表的循环遍历和嵌套的程序文件，写了for循环，还写了在嵌套列表里面查找李四位置的代码

---
+++ 13-07-列表的循环遍历.py
@@ -0,0 +1,24 @@
+# 列表的循环遍历
+# 需求：依次打印列表中的各个数据。
+
+name_list = ['Tom', 'Lily', 'Rose']
+# while循环
+i = 0
+while i < len(name_list):
+    print(name_list[i])
+    i += 1
+
+# for循环
+j = 0
+for j in name_list:
+    print(j)
+#     简单 for 循环开始，它遍历一个字符串列表并打印每一个字符串。
+# for循环其实就是把你标定的序列或者是列表里面的每一个元素，都用来执行一遍循环体代码
+
+for a in range(1, 10):
+    print(a)
+
+roadPointObjList = ['Tom', 'Lily', 'Rose']
+for i in range(0, len(roadPointObjList)):
+    print(roadPointObjList[i])
+
