a4abab6eebe27fa6aba7773c0c620d0c626d0cdf
Threshyong <1047981230@qq.com>
2021-01-03 21:12:43 +0800

    完整的实现了学员管理系统的Python函数设计，当然其中有很多可以改进的地方，以后慢慢加油哦！

--- 20-函数加强-01-应用：学员管理系统.py
+++ 20-函数加强-01-应用：学员管理系统.py
@@ -72,6 +72,33 @@ def shanchu():
             i += 1
 
 
+def xiugai():
+    """修改学员信息函数"""
+    for i in range(0, len(xinxi)):
+        if b in xinxi[i].values():
+            xinxi[i]['姓名'] = input('请输入修改后的姓名:')
+            xinxi[i]['学号'] = input('请输入修改后的学号:')
+            xinxi[i]['宿舍号'] = input('请输入修改后的宿舍号:')
+
+
+def chaxun():
+    """查询学员信息函数"""
+    for i in range(0, len(xinxi)):
+        if b in xinxi[i].values():
+            print(f'要查询的学员信息地址下标是{i}\n查询结果是{xinxi[i]}')
+
+
+def xianshi():
+    """显示学员信息函数（显示全部学员信息）"""
+    for key in xinxi[0].keys():
+        print(key, end='\t\t\t')
+    print()
+    for i in range(0, len(xinxi)):
+        for j in xinxi[i].values():
+            print(j, end='\t\t\t')
+        print()
+
+
 chushihua()  # 输入指令，判断，并执行相应动作
 zhiling = int(input('您要进行的指令是：'))  # 输入的指令都是str类型，但是需要的是int类型，用类型转换函数转换成需要的类型
 xinxi = [{'姓名': '11', '学号': '11', '宿舍号': '11'}, {'姓名': '111', '学号': '111', '宿舍号': '111'}, {'姓名': '1111', '学号': '1111', '宿舍号': '1111'}]  # 创建空列表用来存储学员信息，信息包括姓名，学号，宿舍号。
@@ -89,7 +116,7 @@ while 1:
         print('是否还需要添加学员信息？')
         print('1 = 继续添加')
         print('2 = 停止添加，返回主菜单')
-        caozuo = int(input('请输入1或2来执行相应操作:'))
+        caozuo: int = int(input('请输入1或2来执行相应操作:'))
         if caozuo == 1:
             zhiling = 1
         elif caozuo == 2:
@@ -131,3 +158,58 @@ while 1:
                 zhiling = int(input('您要进行的指令是：'))
 
     """修改学员信息函数"""
+    if zhiling == 3:
+        b = input('请输入学员的学号信息，来判断学员信息是否存在：')
+        if panduan():
+            print('学员信息存在,是否确认执行修改指令')
+            if input('1或0进行选择，1是确认，0是取消:'):
+                xiugai()
+                print('学员信息已经修改')
+                print('是否还需要修改学员信息？')
+                print('1 = 继续修改')
+                print('2 = 停止修改，返回主菜单')
+                caozuo = int(input('请输入1或2来执行相应操作:'))
+                if caozuo == 1:
+                    zhiling = 3
+                elif caozuo == 2:
+                    chushihua()
+                    zhiling = int(input('您要进行的指令是：'))
+                else:
+                    print('输入错误，请重新输入')
+                    continue
+
+    """查询学员信息函数"""
+    if zhiling == 4:
+        b = input('请输入学员的学号信息，来判断学员信息是否存在：')
+        if panduan():
+            print('学员信息存在,查询结果如下:')
+            chaxun()
+            print('是否还需要查询其他学员信息？')
+            print('1 = 继续查询')
+            print('2 = 停止查询，返回主菜单')
+            caozuo = int(input('请输入1或2来执行相应操作:'))
+            if caozuo == 1:
+                zhiling = 4
+            elif caozuo == 2:
+                chushihua()
+                zhiling = int(input('您要进行的指令是：'))
+            else:
+                print('输入错误，请重新输入')
+                continue
+
+    """显示学员信息函数（显示全部学员信息）"""
+    if zhiling == 5:
+        xianshi()
+        print('学员信息显示如上')
+        b = input('按Enter退出显示，回到主菜单')
+        if b == 0:
+            chushihua()
+            zhiling = int(input('您要进行的指令是：'))
+        else:
+            chushihua()
+            zhiling = int(input('您要进行的指令是：'))
+
+    """退出系统函数"""
+    if zhiling == 6:
+        chushihua()
+        zhiling = int(input('您要进行的指令是：'))
